b0da61e53ea3345849abedc74bbc7c001ebce755
Muralisankar <muralisankar321@gmail.com>
2021-12-21 19:32:21 -0600

    Arrays in datastructures

---
+++ src/PracticeDataStructures/Arrays.java
@@ -0,0 +1,23 @@
+package PracticeDataStructures;
+
+public class Arrays {
+	
+	public static void main(String args[]) {
+		
+		int[] intArray =new int[7];
+		
+		intArray[0] = 45;
+		intArray[1] = 49;
+		intArray[2] = 44;
+		intArray[3] = 22;
+		intArray[4] = -10;
+		intArray[5] = 90;
+		intArray[6] = 28;
+		
+		for(int i=0;i<intArray.length;i++) {
+			System.out.println(intArray[i]);
+			
+		}
+	}
+
+}
